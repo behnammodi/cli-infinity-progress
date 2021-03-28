2576eec69f1eba4265a222fe9cf83919c350bbb2
Behnam Mohammadi <itten@live.com>
2021-03-29 03:36:58 +0430

    test(project): add minimum number value for test

--- index.test.js
+++ index.test.js
@@ -2,22 +2,25 @@ const CLIInfinityProgress = require('./index');
 const { delay } = require('tlence');
 
 (async () => {
-  console.log('Test start and stop after 5s');
+  console.log('Test start and stop after 4s');
   const progress = new CLIInfinityProgress();
   progress.start();
-
-  await delay(5000);
-
+  await delay(4000);
   progress.stop();
 
   console.log('Test start with specific config and remove after 1s');
   progress.setSize(20);
   progress.setBarSize(5);
   progress.setRefreshRate(50);
-
   progress.start();
-
   await delay(1000);
-
   progress.remove();
+
+  console.log('Test width minimum size and stop after 3s');
+  progress.setSize(1);
+  progress.setBarSize(1);
+  progress.setRefreshRate(100);
+  progress.start();
+  await delay(3000);
+  progress.stop();
 })();
