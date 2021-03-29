7eddab261fda6eea095713a7699bef045274f0c1
Behnam Mohammadi <itten@live.com>
2021-03-29 14:57:05 +0430

    feat(demo): you can select demo version by command ex npm run demo -- 2

--- demo.js
+++ demo.js
@@ -1,13 +1,43 @@
 const CLIInfinityProgress = require('./index');
 
-(async () => {
-  console.log('This is just a demo of CLIInfinityProgress');
-  const progress = new CLIInfinityProgress();
+const version = process.argv.find((value) => value.length === 1) || '1';
 
-  console.log('\nLoading ...');
-  progress
-    .setSize(30)
-    .setBarSize(10)
-    .setRefreshRate(1000 / 25)
-    .start();
-})();
+if (version === '1') {
+  (async () => {
+    console.log('This is just a demo of CLIInfinityProgress');
+    const progress = new CLIInfinityProgress();
+
+    progress
+      .setHeader('\nLoading ...')
+      .setFooter('Please be patient.')
+      .setSize(30)
+      .setBarSize(10)
+      .setRefreshRate(1000 / 25)
+      .start();
+
+    setTimeout(() => {
+      progress.setFooter('We trying to load content.');
+    }, 3000);
+  })();
+} else if (version === '2') {
+  (async () => {
+    console.log('This is just a demo of CLIInfinityProgress');
+    const progress = new CLIInfinityProgress();
+
+    console.log('\n');
+
+    progress
+      .setHeader('Loading ...')
+      .setFooter('\nPlease be patient, Cab is coming.')
+      .setBarChar('🚕')
+      .setBackgroundChar('_')
+      .setDirectionRightToLeft()
+      .setSize(32)
+      .setBarSize(1)
+      .setRefreshRate(100)
+      .start();
+  })();
+  return;
+} else {
+  console.log(`We can not find version ${version} of demo`);
+}
