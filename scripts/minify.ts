d5824f68719852aa1e4de392e27cd5391a3cdfde
Behnam Mohammadi <itten@live.com>
2021-03-30 23:37:01 +0430

    fix(minify): typo

--- scripts/minify.ts
+++ scripts/minify.ts
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const UglifyJS = require('uglify-js');
 
-const inputPatch = path.join(__dirname, '..', 'index.js');
-const input = fs.readFileSync(inputPatch, 'utf8');
+const inputPath = path.join(__dirname, '..', 'index.js');
+const input = fs.readFileSync(inputPath, 'utf8');
 const { error, code } = UglifyJS.minify(input);
 
 if (error) {
   console.error(error);
 } else {
-  fs.writeFileSync(inputPatch, code);
+  fs.writeFileSync(inputPath, code);
   console.log('Minify Done!');
 }
